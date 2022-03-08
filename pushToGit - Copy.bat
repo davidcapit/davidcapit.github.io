44d2b01205e4ad6be67db1cd99f5b368fd80b929
davidcapit <99508303+davidcapit@users.noreply.github.com>
2022-03-08 12:07:22 +0000

    scheduled commit

---
+++ pushToGit - Copy.bat
@@ -0,0 +1,10 @@
+pushd %~dp0
+@echo on
+IF EXIST "G:\My Drive\GITHUB\davidcapit.github.io\.git\index.lock" DEL /F "G:\My Drive\GITHUB\davidcapit.github.io\.git\index.lock"
+cd G:\My Drive\GITHUB\davidcapit.github.io
+git pull
+git add . 
+git commit -m "scheduled commit"
+git push
+cmd /k
+
